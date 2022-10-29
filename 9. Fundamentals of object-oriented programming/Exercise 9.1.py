977434a129174e773633f648d8b5d96db6136669
vladislavzakatov <vladislav.zakatov@metropolia.fi>
2022-10-29 16:05:30 +0300

    9.1 code added

--- 9. Fundamentals of object-oriented programming/Exercise 9.1.py
+++ 9. Fundamentals of object-oriented programming/Exercise 9.1.py
@@ -1,5 +1,3 @@
-#Exercise 9.1
-
 # Write a Car class that has the following properties: registration number, maximum speed, current speed and travelled distance.
 # Add a class initializer that sets the first two of the properties based on parameter values.
 # The current speed and travelled distance of a new car must be automatically set to zero.
